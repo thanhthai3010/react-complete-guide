4766efb6c9f3df7ac66e425e493a4d34853762be
ThaiNT <thai@emobi.biz>
2020-05-01 22:09:02 +0700

    Module 7 - Video 17 Using PureComponent

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./Cockpit.css";
 
-const cockpit = (props) => {
+const Cockpit = (props) => {
+  // useEffect(() => {
+  //   console.log("[Cockpit.js] useEffect");
+  //   // Can use to call http request ...
+  //   setTimeout(() => {
+  //     alert('Saved data to cloud.')
+  //   }, 1000);
+  // }, []);
+
+  useEffect(() => {
+    console.log("[Cockpit.js] useEffect 2nd");
+    // Can use to call http request ...
+  });
+
+  // we can call as many useEffect as we want.
+
   const assignedClasses = [];
   let btnClass = "";
   if (props.showPersons) {
     btnClass = classes.Red;
   }
 
-  if (props.persons.length <= 2) {
+  if (props.personsLength <= 2) {
     assignedClasses.push(classes.red); // classes = ["red"]
   }
-  if (props.persons.length <= 1) {
+  if (props.personsLength <= 1) {
     assignedClasses.push(classes.bold); // classes = ["red", "bold"]
   }
   return (
@@ -25,4 +40,4 @@ const cockpit = (props) => {
   );
 };
 
-export default cockpit;
+export default React.memo(Cockpit);
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[App.js] constructor");
+  }
+
   state = {
     persons: [
       { id: "123", name: "Max", age: 28 },
@@ -12,6 +17,24 @@ class App extends Component {
     ],
     showPersons: false,
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("[App.js] getDerivedStateFromProps", props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log("[App.js] componentDidMount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[App.js] shouldComponentUpdate");
+    return true;  
+  }
+
+  componentDidUpdate() {
+    console.log("[App.js] componentDidUpdate");
+  }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
@@ -45,6 +68,7 @@ class App extends Component {
   };
 
   render() {
+    console.log("[App.js] render");
     let persons = null;
 
     if (this.state.showPersons) {
@@ -62,7 +86,7 @@ class App extends Component {
         <Cockpit
           title={this.props.appTitle}
           showPersons={this.state.showPersons}
-          persons={this.state.persons}
+          personsLength={this.state.persons.length}
           clicked={this.togglePersonsHandler}
         />
         {persons}
